f1c5df156a8f66a612d84b9358f9bc3149748cbd
kinshuk bageja <kinshukbageja@gmail.com>
2026-04-04 00:56:50 +0530

    updated prisma.js and updated as per the ESM Modul

--- src/models/prisma.js
+++ src/models/prisma.js
@@ -1,7 +1,10 @@
 import pkg from 'pg';
 const { Pool } = pkg;
-import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '@prisma/client';
+
+import { PrismaPg } from '@prisma/adapter-pg';  // ESM — named import works fine
+
+import prismaClientPkg from '@prisma/client';
+const { PrismaClient } = prismaClientPkg;
 
 const connectionString = process.env.DATABASE_URL;
 
